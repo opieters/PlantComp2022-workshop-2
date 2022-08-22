d8174b5e730cbb841f4711a1d4d918f2ab37b2ba
Olivier Pieters <me@olivierpieters.be>
2022-08-22 15:45:25 +0200

    Update blinky.ino

--- src/blinky.ino
+++ src/blinky.ino
@@ -1,1 +1,17 @@
+#include <Wire.h>
+#include <Dwenguino.h>
+#include <LiquidCrystal.h>
 
+void setup()
+{
+  initDwenguino();
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  digitalwrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(500);
+}
